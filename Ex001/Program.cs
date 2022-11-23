4d118f6cfd0150e4d608b24bccc82961e2ab3179
Petr Volkov <118010969+Woolf1982@users.noreply.github.com>
2022-11-23 12:55:02 +0300

    Ex001, 002, 003

---
+++ Ex001/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+ а какое меньшее.
+ a = 5; b = 7 -> max = 7
+ a = 2 b = 10 -> max = 10
+ a = -9 b = -3 -> max = -3
+ */
+
+int a = new int();
+Console.WriteLine("Введите первое число");
+a = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int b = new int();
+Console.WriteLine("Введите второе число");
+b = Convert.ToInt32(Console.ReadLine());
+if (a > max ) max = a;
+if (b > max ) max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
---
+++ Ex003/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+ является ли число чётным (делится ли оно на два без остатка).
+ 4 -> да
+-3 -> нет
+7 -> нет */
+
+
+int userNumber = new int();
+Console.WriteLine("Введите число");
+userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber % 2 == 0)
+    {
+        Console.WriteLine("Да (введено четное число)");
+    }
+else
+    {
+    Console.WriteLine("Нет (введено нечетное число)");
+    }
