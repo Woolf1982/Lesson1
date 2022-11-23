c911ac00f5c85822037d4fec2f2bb01bf7543590
Petr Volkov <118010969+Woolf1982@users.noreply.github.com>
2022-11-23 12:56:38 +0300

    Ex004

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), 
+ а на выходе показывает все чётные числа от 1 до N.
+ 5 -> 2, 4
+ 8 -> 2, 4, 6, 8 */
+
+
+int N = new int();
+Console.WriteLine("Введите число");
+N = Convert.ToInt32(Console.ReadLine());
+int i=0;
+while (i < N - 1)
+{
+    i += 2;
+    Console.WriteLine(i);
+}
+
